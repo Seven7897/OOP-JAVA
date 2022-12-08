c94f8f7879309a826e8ac92165c0642a3e9d9515
Anatoly <107132732+Seven7897@users.noreply.github.com>
2022-12-08 18:21:12 +0300

    home1

---
+++ Relationship.java
@@ -0,0 +1,14 @@
+package GenealogicalTreeResearch;
+
+class Relationship {
+    public static int parent = 1;
+    public static int child = 2;
+    public static int uncle = 3;
+    public static int aunt = 4;
+    public static int cousin = 5;
+    public static int parentInLaw = 6;
+    public static int nephew = 7;
+    public static int niece = 8;
+    public static int brother = 9;
+    public static int sister = 10;
+}
